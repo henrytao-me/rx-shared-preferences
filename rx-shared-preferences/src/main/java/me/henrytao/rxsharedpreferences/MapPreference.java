415622b8755d749e3d4bd2dda2911127b40d3af6
Henry Tao <hi@henrytao.me>
2015-12-03 10:07:35 +0700

    Init MapPreference

---
+++ rx-shared-preferences/src/main/java/me/henrytao/rxsharedpreferences/MapPreference.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 "Henry Tao <hi@henrytao.me>"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.henrytao.rxsharedpreferences;
+
+import org.json.JSONObject;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by henrytao on 11/22/15.
+ */
+public class MapPreference extends JSONObjectPreference {
+
+  public MapPreference(SharedPreferences sharedPreferences) {
+    super(sharedPreferences);
+  }
+
+  
+
+  @Override
+  protected JSONObject getValue(String key, JSONObject defValue) {
+    String jsonString = mSharedPreferences.getString(key, null);
+    try {
+      return new JSONObject(jsonString);
+    } catch (Exception e) {
+      return defValue;
+    }
+  }
+
+  @Override
+  protected void putValue(String key, JSONObject value) {
+    try {
+      String jsonString = value.toString();
+      mSharedPreferences.edit().putString(key, jsonString);
+    } catch (Exception ignore) {
+    }
+  }
+}
